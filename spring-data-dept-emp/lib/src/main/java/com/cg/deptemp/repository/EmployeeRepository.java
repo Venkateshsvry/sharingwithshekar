567c8c7bf2f935e7c21d891c6df8bb4eb61db723
Sekhar Makkella <103580979+MAKKELLASEKHAR@users.noreply.github.com>
2024-03-29 18:05:58 +0530

    Delete spring-data-dept-emp directory

--- spring-data-dept-emp/lib/src/main/java/com/cg/deptemp/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cg.deptemp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.deptemp.entities.Employee;
-
-public interface EmployeeRepository  extends JpaRepository<Employee,Integer>{
-
-}
